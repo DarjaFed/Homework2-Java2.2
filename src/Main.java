450f7559ca8e844d703fda0adf3148db4f0f54c5
DarjaF <carrot.the.oblivion@gmail.com>
2025-02-22 22:47:42 +0300

    .gitignore added

--- src/Main.java
+++ src/Main.java
@@ -2,14 +2,15 @@
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+        int initialBalance = 100; // сумма на счету
+        int addend = 1200; // пополнение счета
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
+        int bonus = 0;
+        if (addend > 1000) { // бонус дается за сумму выше 1000
+            bonus = addend / 100; // бонус дается за каждые 100 руб
+        }
+        int finalBalance = initialBalance + addend + bonus;
+        System.out.println("Итого:" + finalBalance);
+        System.out.println("Бонус руб.: " + bonus);
         }
     }
-}
